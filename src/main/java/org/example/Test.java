8efacd42c48e3972456f5bb3d199120f7ae0667e
nnga <nnga3363@gmail.com>
2024-02-07 11:39:05 +0700

    add out

--- src/main/java/org/example/Test.java
+++ src/main/java/org/example/Test.java
@@ -5,5 +5,6 @@ import org.w3c.dom.ls.LSOutput;
 public class Test {
     public static void Test1(){
         System.out.println("Nguyen Thi Thuy Nga");
+        System.out.println("Giang Vu Hoang Tuan");
     }
 }
