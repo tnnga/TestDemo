a889ff6526bf69d35833a4098218b383fed48664
nnga <nnga3363@gmail.com>
2024-02-16 15:06:20 +0700

    add new print

--- src/main/java/org/example/Test.java
+++ src/main/java/org/example/Test.java
@@ -7,5 +7,6 @@ public class Test {
         System.out.println("Nguyen Thi Thuy Nga");
         System.out.println("Giang Vu Hoang Tuan");
         System.out.println("Nguyen Viet Khoi");
+        System.out.println("Le Cong Dung");
     }
 }
