9094659d2f72f06eec392480152abfe6290a33c0
nnga <nnga3363@gmail.com>
2024-02-22 17:15:46 +0700

    add one line

--- src/main/java/org/example/Test.java
+++ src/main/java/org/example/Test.java
@@ -5,14 +5,10 @@ import org.w3c.dom.ls.LSOutput;
 public class Test {
     public static void Test1(){
         System.out.println("Nguyen Thi Thuy Nga");
-        System.out.println("Nguyen Viet Khoi");
-        System.out.println("Le Cong Dung");
-        System.out.println("Vo Tran Duc Khoi");
-
-        System.out.println("Happy Birthday Thuy Nga");
-        System.out.println("Happy birthday to me");
 
         System.out.println("Pipeline is okay");
         System.out.println("Nguyen Thi Thuy Nga");
+
+        System.out.println("Add new line");
     }
 }
